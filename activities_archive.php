a010d6b5b9c0c378bf9ef6f25b4941c8b2991c7d
ranchen <ranchen@seas.upenn.edu>
2013-02-23 23:31:04 -0500

    Update activities_archive.php

--- activities_archive.php
+++ activities_archive.php
@@ -21,7 +21,7 @@
 <?php
 
 //Connect to MySQL server
-$mysqli = new mysqli('EDITTED OUT');
+$mysqli = new mysqli('EDITED OUT');
 if (mysqli_connect_errno()) {
   printf("Can't connect to MySQL Server. Errorcode: %s\n", mysqli_connect_error());
   exit;
