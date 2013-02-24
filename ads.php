568ece95f6db149a14297a7d80a05c94f88abe5b
ranchen <ranchen@seas.upenn.edu>
2013-02-23 23:40:38 -0500

    Update ads.php

--- ads.php
+++ ads.php
@@ -92,7 +92,7 @@
     <?php
     
     //Connect to MySQL server
-    $mysqli = new mysqli('ecccanet.ipagemysql.com', 'eccca', 'eccca123', 'eccca');
+    $mysqli = new mysqli('EDITED OUT');
     if (mysqli_connect_errno()) {
       printf("Can't connect to MySQL Server. Errorcode: %s\n", mysqli_connect_error());
       exit;
